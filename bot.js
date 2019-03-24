a7f41803894c96f13af2928d59fc5bb270f05006
ajinla3bo <46244753+ajinla3bo@users.noreply.github.com>
2019-03-24 19:04:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("559439686364299264")
+let channel =     client.channels.get("524683703910137869")
 setInterval(function() {
 channel.send(`hi hello hi hello`);
 }, 30)
